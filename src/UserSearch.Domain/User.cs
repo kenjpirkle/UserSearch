019f7aeae781f30d64fbb243baa380635ba53cde
Ken Pirkle <kenjpirkle@gmail.com>
2023-02-01 16:24:42 +0000

    Add domain entity

---
+++ src/UserSearch.Domain/User.cs
@@ -0,0 +1,10 @@
+namespace UserSearch.Domain;
+
+public sealed class User
+{
+    public long Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Gender { get; set; }
+}
